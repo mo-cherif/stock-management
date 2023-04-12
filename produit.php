e61c1600b484ffa44862af58ee9fd71b9e216c61
mo-cherif <mocherifh0@gmail.com>
2023-04-12 10:59:36 +0000

    x

--- produit.php
+++ produit.php
@@ -3,45 +3,66 @@
         private $reference;
         private $designation;
         private $prix;
-        private $quantite_stock
-    }
-
-    public function getReference() {
-        return $this->reference;
-    }
-
-    public function setReference($ref) {
-        $this->reference = $ref;
-    }
-
-    public function getDesignation() {
-        return $this->designation;
-    }
-
-    public function setReference($des) {
-        $this->designation = $des;
-    }
-
-    public function getPrix() {
-        return $this->prix;
-    }
-
-    public function setReference($prix) {
-        $this->prix = $prix;
-    }
-
-    public function getQuantiteStock() {
-        return $this->quantite_stock;
-    }
-
-    public function setReference($quantite_stock) {
-        $this->quantite_stock = $quantite_stock;
-    }
-
+        private $quantite_stock;
     
 
+        public function __construct() {
+
+        }
+
+        public function getReference() {
+            return $this->reference;
+        }
+
+        public function setReference($ref) {
+            $this->reference = $ref;
+        }
+
+        public function getDesignation() {
+            return $this->designation;
+        }
+
+        public function setDesignation($des) {
+            $this->designation = $des;
+        }
+
+        public function getPrix() {
+            return $this->prix;
+        }
+
+        public function setPrix($prix) {
+            $this->prix = $prix;
+        }
+
+        public function getQuantiteStock() {
+            return $this->quantite_stock;
+        }
+
+        public function setQuantiteStock($quantite_stock) {
+            $this->quantite_stock = $quantite_stock;
+        }
+
+        public function create() {
+
+        }
+
+        public function readOne() {
+
+        }
+        public function read() {
+
+        }
 
 
+        public function update() {
 
+        }
+
+        public function delete() {
+            
+        }
+
+
+    }
 
 ?>
